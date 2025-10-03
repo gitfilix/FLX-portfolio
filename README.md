b03f62a952a107a6dde28d02091877f6a6992f43
gitfilix <runlevel07@gmail.com>
2025-10-03 15:20:15 +0200

    initial setup: cleaned nextjs project

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # FLX-portfolio
+
+lets have a shiny new portfolio for my Work-life
+
+# Tools used
+
+React, Next JS, FramerMotion
+
+
+#
---
+++ flx-portfolio/src/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Outfit } from "next/font/google";
+import "./globals.css";
+
+const outfit = Outfit({
+  subsets: ["latin"],
+});
+
+
+export const metadata: Metadata = {
+  title: "FLX Portfolio",
+  description: "Portfolio website for FLX",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${outfit.className}  antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
