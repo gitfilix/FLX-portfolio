551e108edae30cdde33aec598c4e11c15fe66e38
gitfilix <runlevel07@gmail.com>
2025-10-03 20:46:55 +0200

    P: hero section and aurora effect with Framermagic

--- flx-portfolio/src/app/page.tsx
+++ flx-portfolio/src/app/page.tsx
@@ -1,11 +1,38 @@
 'use client'
 import Hero from "@/components/Hero"
+import { motion, useMotionTemplate, useMotionValue, animate } from 'framer-motion'
+import { useEffect } from 'react'
+
 
 export default function Home() {
+  // aurora background effect magic fuckery: 
+  // color Range changes value in the animation useEffect 
+  const colorRange = ['#12dFAA', '#1E67C6', '#AA335C', '#DD335C']
+  const colorChange = useMotionValue(colorRange[0])
+  const backgroundImage = useMotionTemplate`radial-gradient(100% 180% at 50% 0%, #020217 25%, ${colorChange})`
+
+  useEffect(() => {
+    animate(colorChange, colorRange, {
+      ease: 'easeInOut',
+      duration: 12,
+      repeat: Infinity,
+      repeatType: 'mirror'
+    }
+    )
+  }, [])
+
+
   return (
-    <div>
-      test page
-      <Hero />
-    </div>
+    <section>
+      <motion.div
+      style={{
+         backgroundImage
+      }}
+      className='relative grid min-h-screen place-content-center overflow-hidden bg-gray-950 text-gray-200'
+      >
+        test page
+        <Hero />
+      </motion.div>
+    </section>
   )
 }
--- flx-portfolio/src/components/Hero.tsx
+++ flx-portfolio/src/components/Hero.tsx
@@ -1,28 +1,67 @@
 'use client'
+import { useMotionValue, useTransform, motion, useSpring } from 'framer-motion'
 import Image from "next/image"
+import { useState } from 'react'
+
 
 const Hero = () => {
+  // framerMotion magic starts here
+  const [windowOffset, setWindowOffset] = useState({ innerWidth: 0, innerHeight: 0})
+  const [mouseMove, setMouseMove] = useState(false)
+  const x = useMotionValue(0)
+  const y = useMotionValue(0)
+
+  const handleMouseMove = (e) => {
+    const { clientX, clientY } = e
+    x.set(clientX)
+    y.set(clientY)
+  }
+
+  // get the windowsize inner of the client and start the setMouseMove true
+  const handleMouseEnter = () => {
+    setWindowOffset({innerWidth: window.innerWidth, innerHeight: window.innerHeight })
+    setMouseMove(true)
+  }
+
+  const { innerWidth, innerHeight } = windowOffset
+
+  // useSpring from FramerMotion
+  const xSpring = useSpring(x, {stiffness: 100, damping: 10 })
+  const ySpring = useSpring(y, {stiffness: 100, damping: 10 })
+
+
+  // if the mouse moves on the y axis: we transform from -30degrees to + 30degrees -> Tilt effect
+  const rotateY = useTransform(xSpring, [0, innerWidth], [-30, 30])
+  // if the mouse moves on the x axis: we transform from 10degrees to -10 degrees -> horizontal effect
+  const rotateX = useTransform(ySpring, [0, innerHeight], [10, -50])
+
+
   return (
-      <div className="h-screen grid, place-items-center">
+      <div className="h-screen grid, place-items-center" onMouseMove={handleMouseMove} onMouseEnter={handleMouseEnter}>
           <div>
               <div className='flex flex-col items-center justify-center  gap-y-3 font-light capitalize'>
-                  <div className='flex item-center justify-center'>
+                  <motion.div className='flex item-center justify-center' style={{
+                    rotateX: mouseMove ? rotateX : 0,
+                    rotateY: mouseMove ? rotateY : 0,
+                    transition: '0.1s'
+                  }}>
                       <Image 
-                        src={'/person.png'} 
+                        // src={'/person.png'} 
+                        src={'/me_test.png'} 
                         alt='me' width={400} 
                         height={400} 
                         priority={true}
                         className='h-auto w-[150px]'
                         />
-                      <span className="absolute text-3xl, font-semibold text-white">Hi</span>
-                  </div>
+                      <span className="absolute text-3xl, font-semibold text-white">Hi There 👋</span>
+                  </motion.div>
                   <h1 className='text-center text-3xl font-bold tracking-wider text-gray-600'>My Name is Felix 😊</h1>
                   <p className='text-lg tracking-wider text-gray-700'>I like Animations..</p>
               </div>
               <div>
-                <a href='#'>Icon</a>
+               <a href='#' className='mt-7 block w-max rounded-lg bg-red-400 px-3 py-1 font-light capitalize tracking-wider text-white hover:bg-red-800 transition-colors'>my Projects</a>
               </div>
-              <a href='#' className='mt-7 block w-max rounded-lg bg-red-400 px-3 py-1 font-light capitalize tracking-wider text-white hover:bg-red-800 transition-colors'>Talk to me</a>
+              <a href='#' className='mt-7 block w-max rounded-lg bg-red-400 px-3 py-1 font-light capitalize tracking-wider text-white hover:bg-red-800 transition-colors'>Contact me</a>
           </div>
       </div>
   )
