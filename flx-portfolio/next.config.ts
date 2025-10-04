29f1bff99b35bde680c7e81a9282e246a38a78e7
gitfilix <runlevel07@gmail.com>
2025-10-04 11:39:26 +0200

    Portfolio: render-deploy test 1

--- flx-portfolio/next.config.ts
+++ flx-portfolio/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
-
+// export as static for render.com hosting - for now
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export"
 };
 
 export default nextConfig;
