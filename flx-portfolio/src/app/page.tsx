37b7b05cbbf9d35426ef823e4e63f5c041e27419
gitfilix <runlevel07@gmail.com>
2025-11-24 21:10:40 +0100

    Page better aurora effect

--- flx-portfolio/src/app/page.tsx
+++ flx-portfolio/src/app/page.tsx
@@ -17,19 +17,34 @@ export default function Home() {
 
   // aurora background effect magic fuckery: 
   // color Range changes value in the animation useEffect 
-  const colorRange = ['#861236', '#1E67C6', '#AA335C', '#DD335C', '#322D56']
+  const colorRange = ['#861236', '#1E67C6', '#AA335C', '#DD335C', '#3340A7']
   const colorChange = useMotionValue(colorRange[0])
-  const backgroundImage = useMotionTemplate`radial-gradient(100% 180% at 50% 0%, #010207 25%, ${colorChange})`
+  const gradientSize = useMotionValue(150)
+  const gradientSpread = useMotionValue(280)
+  
+  // const backgroundImage = useMotionTemplate`radial-gradient(100% 220% at 0% 30%, #010207 25%, ${colorChange})` // org
+  const backgroundImage = useMotionTemplate`radial-gradient(${gradientSize}% ${gradientSpread}% at 10% 50%, #010101  22%, ${colorChange})`
 
   // TODO: Bring this cool effects back
   useEffect(() => {
     animate(colorChange, colorRange, {
       ease: 'easeInOut',
+      duration: 25,
+      repeat: Infinity,
+      repeatType: 'mirror'
+    })
+    animate(gradientSize, [150, 100, 180], {
+      ease: 'easeIn',
+      duration: 24,
+      repeat: Infinity,
+      repeatType: 'mirror'
+    })
+    animate(gradientSpread, [190, 210, 250, 280], {
+      ease: 'easeInOut',
       duration: 12,
       repeat: Infinity,
       repeatType: 'mirror'
-    }
-    )
+    })
   }, [])
 
 
@@ -46,7 +61,7 @@ export default function Home() {
             <Hero />
             <AboutMe />
             <Education />
-            {/* <Skills /> */}
+            <Skills />
             <Experience />
             <Questions />
           </div>
@@ -54,9 +69,7 @@ export default function Home() {
     </section>
   )
 }
-
-
-           {/* <div className='w-full p-6
+{/* <div className='w-full p-6
                   sm:bg-amber-600 
                   md:bg-amber-900
                   lg:bg-blue-400
